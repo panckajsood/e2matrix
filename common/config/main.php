cd6edfa0613d065275187bd9305a852131494373
panckajsood <pankaj.sood205@gmail.com>
2017-04-08 13:21:30 +0530

    database set up

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,22 @@
 <?php
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=e2matrix_app',
+            'username' => 'pankajsood',
+            'password' => 'f.V![]WGTMm3',
+            'tablePrefix' => 'e2matrix_',
+            'charset' => 'utf8',
+        ],
+        
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
         ],
     ],
 ];
